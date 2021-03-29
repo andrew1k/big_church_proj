58465f348a2265cf36dc10cec894145d11447f22
Andrew Koshelev <ndrei61999@gmail.com>
2021-03-29 00:58:47 -1000

    Init proj

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+ <hello-page />
+</template>
+
+<script>
+import HelloPage from './components/HelloPage.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloPage
+  }
+}
+</script>
+
+<style src="./app.css">
+
+</style>
